6a844a41b7d9c099db8d841ed6ccc976fb408367
Cit <shavinsept@gmail.com>
2022-08-30 16:53:56 +1000

    Removed sensitive information

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { CustomClient } from "./Structure/index.js";
 const client = new CustomClient({
     data: {
-        token: "OTg1NDY0NjE2ODcwMDkyODEw.GDqMVp.LXngSpx2-BOqw8OsSK-ZJvEk6m4RR0X5C8LfSs",
-        database: "mongodb+srv://cit:123@zen.huiaw.mongodb.net/Zen?retryWrites=true&w=majority",
+        token: "TOKEN",
+        database: "URL",
         handlers: { commands: "./dist/commands", events: "./dist/events" },
-        developerGuilds: ["900965997215895604"]
+        developerGuilds: ["id1", "id2"]
     },
     intents: 131071
 });
