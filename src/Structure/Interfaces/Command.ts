7a86b53ce94ff5289b21b90b78452a4643a2437b
Cit <shavinsept@gmail.com>
2022-09-26 17:14:40 +1000

    Fixed incorrect parameters

--- src/Structure/Interfaces/Command.ts
+++ src/Structure/Interfaces/Command.ts
@@ -1,9 +1,9 @@
-import { ChatInputApplicationCommandData, CommandInteraction, PermissionResolvable } from "discord.js";
+import { ChatInputApplicationCommandData, ChatInputCommandInteraction, PermissionResolvable } from "discord.js";
 import { CustomClient } from "../Classes/Client.js";
 
 export interface Command extends ChatInputApplicationCommandData {
     developer?: boolean;
     botOwnerOnly?: boolean;
     botPermissions?: PermissionResolvable[];
-    execute: (interaction: CommandInteraction, client: CustomClient) => any;
+    execute: (interaction: ChatInputCommandInteraction, client: CustomClient) => any;
 }
